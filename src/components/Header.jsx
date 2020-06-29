509a4c95f493b74b4f59739e4aa2ea0f26502b5c
Dmitriy Vladimirovich <mortargrind91@gmail.com>
2020-06-29 14:31:37 +0300

    added axios and propTypes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import logoSvg from './assets/img/pizza-logo.svg';
+import logoSvg from '../assets/img/pizza-logo.svg';
+import Button from './Button';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src={logoSvg} alt="Pizza logo" />
-          <div>
-            <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src={logoSvg} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
           </div>
-        </div>
+        </Link>
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
+          <Button className="button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter"></div>
             <svg
@@ -45,7 +49,7 @@ const Header = (props) => {
               />
             </svg>
             <span>3</span>
-          </a>
+          </Button>
         </div>
       </div>
     </div>
